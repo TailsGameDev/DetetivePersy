93cc44d7db8b11be57d3895d0f0b9a128b9c2feb
takeshi99 <takeshi.yamashita878@gmail.com>
2019-09-17 15:56:27 -0300

    Mecanica de investigação com todos os prefabs, esqueci na ultima

---
+++ Fungus/Assets/ClickCanvas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickCanvas : MonoBehaviour
+{
+    public GameObject prova;
+    public GameObject cnvas;
+    public Text texto;
+
+    private void OnMouseDown()
+    {
+        prova.SetActive(true);
+        cnvas.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        
+    }
+    public void Botao() {
+        prova.SetActive(true);
+        cnvas.SetActive(false);
+        
+
+    }
+    public void Escreve(string texto) {
+        this.texto.text = texto;
+
+    }
+}
---
+++ Fungus/Assets/onCliked.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onCliked : MonoBehaviour
+{
+    public string Descriçao = "Descriçao do objeto";
+    public GameObject cnvas;
+
+    private void OnMouseDown()
+    {
+        cnvas.SetActive(true);
+        cnvas.SendMessage("Escreve",Descriçao);
+        this.gameObject.SetActive(false);
+    }
+
+    
+    
+}
