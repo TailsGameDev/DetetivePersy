ba0d6751f73b8865868f6ba7fa9c149d1a4b8845
franko <franco@edlucca.pro.br>
2019-09-19 16:25:30 -0300

    blocoDeNotasfranco

--- Fungus/Assets/onCliked.cs
+++ Fungus/Assets/onCliked.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class onCliked : MonoBehaviour
 {
+    bool japrintado = false;
     public string Descriçao = "Descriçao do objeto";
     public GameObject cnvas;
-
+    public Text Bloco;
     private void OnMouseDown()
     {
         cnvas.SetActive(true);
         cnvas.SendMessage("Escreve",Descriçao);
+        if (japrintado == false)
+        { 
+            Bloco.text = Bloco.text + "\n" + Descriçao;
+        }
         this.gameObject.SetActive(false);
+        japrintado = true;
     }
 
     
