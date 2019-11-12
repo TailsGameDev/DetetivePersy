0a7cdc09204abfa7e014ad81fb6cc4a9247a77ca
Franko12345 <franco@delucca.pro.br>
2019-11-12 16:24:27 -0300

    mapo

---
+++ Fungus/Assets/Scripts/mapo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mapo : MonoBehaviour
+{
+    public Scene duro;
+    // Start is called before the first frame update
+
+    public void OnMouseDown()
+    {
+        //  SceneManager.LoadScene(duro.name);
+        print("hahala");
+        Debug.Log("Entra cena 01");
+    }
+}
